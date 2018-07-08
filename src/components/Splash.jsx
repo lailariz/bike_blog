2a9e7867898ac063d5217bc56632583ff026da41
Ze'ev Lailari <lailari.zeev@gmail.com>
2018-07-08 11:19:43 +0200

    Try deploy script

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -6,14 +6,12 @@ class Splash extends Component {
   render () {
     return (
       <Jumbotron className="p-0 text-white rounded bg-dark centered-bg-img" id="jumbotron-splash">
-				<Link className="unstyled-link" to="/post">
-					<Link to="/post/07-01-2018">
-            <div className="centered-bg-img bg-img"></div>
-          </Link>
+				<Link to="/post/07-01-2018">
+          <div className="centered-bg-img bg-img"></div>
 					<div className="splash-text-container p-3 p-lg-5 mx-auto">
-            <h5 className="title-text-sm">June 24, 2018</h5>
+            <h5 className="title-text-sm">July 1, 2018</h5>
             <br />
-						<h3 className="title-text">Post Title</h3>
+						<h3 className="title-text">Traveling to Europe: Kindness in Barcelona</h3>
 					</div>
 				</Link>
       </Jumbotron>
---
+++ src/posts/07-01-2018.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    const date "07-01-2018";
+
+    return (
+      <div>
+        <h1>Traveling to Europe: Kindness in Barcelona</h1>
+        <div className="post-content">
+          <p>Ze’ev and I have successfully arrived in our first destination in Europe: Barcelona. While Ze’ev has already been in Barcelona for a couple days to attend a workshop at the CCCB, I am only here for 4 days as a layover before we both head to Croatia. Getting ourselves to this point took countless hours of researching, planning, deassembling bikes and packing 2 months of gear into our bike boxes. We will be living off of everything we are bringing, so it was crucial that we took the time to think through our plans.  We will also be carrying everything we bring on our bikes, so we needed to make sure we were only bringing what was necessary. Finally, everything had to fit in one giant bike box (including our bikes) and so we made sure that it would all fit and also be under the 50lb checked bag weight limit.</p>
+    	<p>We arrived in Barcelona with a sense of relief that (most) of the planning was done and the adventures were beginning. More importantly, I am feeling overwhelmed with kindness and generosity from the people who helped us get here and those we’ve met since arriving. Traveling here by myself was incredibly easy and my stay in Barcelona has been incredible, and i would not be feeling this way without those who have helped out along the way. Shout out to Emily for helping me pack and organize, to grandma for cooking and showing me unconditional support and excitement, and Sonia for driving me to the Oakland airport on Tuesday afternoon.</p>
+    	<p>Upon landing in Barcelona, I had to get myself and my 50lb bike box to my Airbnb in el centro. With the goal of traveling cheaply and relying on the help of others, I decided to take public transportation. After the first bus ride from the airport to Plaza España, I was just 1km to the Airbnb. Walking was definitely my preferred method of transportation but I quickly realized it’d be pretty painful with the bike box. Within just seconds of standing on the sidewalk and contemplating my next move, a friendly man came up to me and asked in Spanish “a dónde vas”? He helped me carry my box into the metro station commenting that it was very heavy. I successfully took the metro one stop while receiving lots of funny but friendly stares. When exiting the elevator that took us to ground level, the 4 other women in the elevator all picked up a handle on the box and help me move it to the crosswalk. In just this half hour, I was amazed at how kind and friendly the Barcelona people were.</p>
+    	<p>Since Ze’ev was in Barcelona before me, he had befriended his Airbnb hosts, a lovely and engaging couple from Colombia, Ricardo and Katlyn. They invited us to Barceloneta Beach on Thursday morning for an 8am workout. Obviously we went. They also invited us to go to the bar with them to watch the Colombia v Senegal World Cup soccer game that afternoon at 4pm. Ze’ev couldn’t make it cause he had class, but I was thrilled to go with them. The bar was overflowing with Colombians in yellow jerseys cheering for their country. When Colombia scored and won the game, the room erupted with support and chants. It was sweet. Once again, I felt so lucky to have met these inviting and generous people in Barcelona.</p>
+			<p>In a couple days we are heading to Dubrovnik, Croatia to start our bike journey across Eastern Europe. Part of the reason why Ze’ev and I both like bike touring is because we get to interact with and meet people we wouldn’t otherwise talk to. People ask questions about our bikes and route, and we ask locals questions about things to see, roads to take, and places to eat. In doing so, we get to learn about their culture and experience acts of kindness that often get overlooked if you’re in a car or walking. We’re so excited to start our adventure and meet new people, see new things, and learn how kind people can be.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Post;
