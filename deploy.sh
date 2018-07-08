6296773572ef031bc5225c8f4afadc9224a50435
Ze'ev Lailari <lailari.zeev@gmail.com>
2018-07-08 11:26:13 +0200

    Deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# npm run build
+npm run build
 cd ~/personal/repos/bike_blog
 mkdir ~/tmp_build
 mv build ~/tmp_build
 git checkout gh-pages
 mv ~/tmp_build/* .
 rm -rf ~/tmp_build
-# git add .
-# git commit -m "Site updated -- deploying new version"
-# git push origin gh-pages
-# git checkout master
+git add .
+git commit -m "Site updated -- deploying new version"
+git push origin gh-pages
+git checkout master
 
