7298531275e83496e3af4550b1f6b67a9b7be039
Ze'ev Lailari <lailari.zeev@gmail.com>
2018-07-10 19:24:58 +0200

    Add email in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,7 @@ class About extends Component {
         <div className="row">
           <div className="col-sm-12 col-md-6">
             <p>Maddie and Ze'ev are biking across Europe. They started in Dubrovnik and, after a quick stop in Montenegro, will end up in Berlin at the beginning of August.</p>
+            <p>If you would like to get in touch with us for any reason, please send an email to <strong>wheeliegoodfriends@gmail.com</strong></p>
           </div>
           <div className="col-sm-12 col-md-6">
             <img className="about-pic" src={pic} alt='Us' />
