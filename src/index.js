ead89935dbcb7dadc533c651ba01eb0a0b3cf86e
Ze'ev Lailari <lailari.zeev@gmail.com>
2018-07-25 00:56:29 +0200

    Site updated -- deploying new version

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/lightbox2/dist/css/lightbox.min.css';
 import '../node_modules/lightbox2/dist/js/lightbox.min.js';
+
+ReactGA.initialize('UA-122808286-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <BrowserRouter>
